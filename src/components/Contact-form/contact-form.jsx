dbd0f82898e394dce0450b7f75d45ea339b3e53c
Aniket Jha <>
2022-11-10 00:13:36 +0530

    forth commit

--- src/components/Contact-form/contact-form.jsx
+++ src/components/Contact-form/contact-form.jsx
@@ -39,7 +39,7 @@ const ContactForm = () => {
         <div className="row">
           <div className="col-lg-6">
             <div className="form md-mb50">
-              <h4 className="fw-700 color-font mb-50">Get In Touch.</h4>
+              <h4 className="fw-700 color-font mb-50">Get  In Touch.</h4>
               <Formik
                 initialValues={{
                   name: "",
